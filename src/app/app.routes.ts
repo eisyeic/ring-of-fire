82b467fc9d2227cb542ea09c8f137935d60b6ae4
eisyeic <davideisenbarth@gmail.com>
2025-12-24 00:26:46 +0100

    create startscreen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { StartscreenComponent } from './src/app/startscreen/startscreen.component';
+import { GameComponent } from './src/app/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StartscreenComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+];
