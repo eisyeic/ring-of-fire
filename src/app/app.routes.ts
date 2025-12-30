89e5c30277ffb04a9c4dab380a6388a242e47b88
eisyeic <davideisenbarth@gmail.com>
2025-12-30 23:23:38 +0100

    Integrate master branch - complete Ring of Fire game

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+import { GameComponent } from './game/game.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: StartscreenComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+];
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule, PlayerComponent],
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true;
+     
+      setTimeout(() => {
+         this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+}
